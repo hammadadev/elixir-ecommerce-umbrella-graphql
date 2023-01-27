53d4c26ede8a976c71cb1daef4679707c076ace3
Hammad Ahmed <hammadahmed202581@gmail.com>
2023-01-27 20:13:54 +0500

    Dockerize application with docker-compose - Add docker file to support docker development.

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :ecommerce, Ecommerce.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "ecommerce_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -21,7 +21,7 @@ config :ecommerce, Ecommerce.Repo, migration_timestamps: [type: :utc_datetime]
 config :ecommerce_web, EcommerceWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
---
+++ entrypoint_dev.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Docker entrypoint script.
+
+# Wait until Postgres is ready
+mix deps.get
+echo "Testing if Postgres is accepting connections. {$PGHOST} {$PGPORT} ${PGUSER}"
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+# Create, migrate, and seed database if it doesn't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  mix ecto.create
+  mix ecto.migrate
+  echo "Database $PGDATABASE created."
+fi
+
+exec mix phx.server
